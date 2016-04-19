6b6c8556e039b55a5fd541cbee0b98c943e8dac9
weddy5132 <weddy5132@163.com>
2016-04-19 15:20:41 +0800

    Added files via upload

--- helper.R
+++ helper.R
@@ -1,4 +1,6 @@
-getRaw <- function () {
-data<-read.table(url("https://raw.githubusercontent.com/weddy5132/escyc/master/escsj.txt"),sep="",header=TRUE)
+getRaw <- function(){
+data<-read.table("https://raw.githubusercontent.com/weddy5132/escyc/master/escsj.txt",sep="",header=TRUE)
 data
 }
+
+
